5d726e945e9cc0c71661c74331feb55fca9fff59
Jonathan Dawe <JDawe@esriuk.com>
2021-05-04 08:34:06 +0100

    initial commit

---
+++ snowpack.config.js
@@ -0,0 +1,11 @@
+// snowpack.config.js
+module.exports = {
+    mount: {
+        "node_modules/@arcgis/core/assets": {url: "/assets", static: true, resolve: false},
+        "src": "/",
+    },
+    plugins: [["@snowpack/plugin-webpack"]],
+    buildOptions:{
+        baseUrl:"./"
+    }
+};
